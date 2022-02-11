63da96ad5952e8622bbf2e833eb7c9718d5c53a4
Your Name <you@example.com>
2022-02-11 18:31:59 +0300

    login with google

--- frontend/mern-2/src/pages/Login.js
+++ frontend/mern-2/src/pages/Login.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { auth } from "../firebase";
+import { auth,googleAuthProvider  } from "../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
-import { MailOutlined } from "@ant-design/icons";
-import { useDispatch } from "react-redux";
 
+import { useDispatch } from "react-redux";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 const Login = ({ history }) => {
   const [email, setEmail] = useState("gqlreactnode@gmail.com");
   const [password, setPassword] = useState("123456");
@@ -36,6 +36,42 @@ const Login = ({ history }) => {
       setLoading(false);
     }
   };
+
+
+
+// LOGIN WITH GOOGLE
+
+const googleLogin = async () => {
+  auth
+    .signInWithPopup(googleAuthProvider)
+    .then(async (result) => {
+      const { user } = result;
+
+      // user token
+      const idTokenResult = await user.getIdTokenResult();
+      dispatch({
+        type: "LOGGED_IN_USER",
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.push("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      toast.error(err.message);
+    });
+};
+
+
+
+
+
+
+
+
+
 
   const loginForm = () => (
     <form onSubmit={handleSubmit}>
@@ -80,8 +116,28 @@ const Login = ({ history }) => {
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h4>Login</h4>
+        {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Login</h4>
+          )}
           {loginForm()}
+
+          <Button
+            onClick={googleLogin}
+            type="danger"
+            className="mb-3"
+            block
+            shape="round"
+            icon={<GoogleOutlined />}
+            size="large"
+          >
+            Login with Google
+          </Button>
+
+
+
+
         </div>
       </div>
     </div>
